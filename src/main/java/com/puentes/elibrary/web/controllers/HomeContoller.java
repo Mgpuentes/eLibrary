962ff58c677cb293e3cdc760c46b9a435a75e37c
MATT\Matthew <mpuent1@students.towson.edu>
2018-03-27 13:06:12 -0400

    added css and js

--- src/main/java/com/puentes/elibrary/web/controllers/HomeContoller.java
+++ src/main/java/com/puentes/elibrary/web/controllers/HomeContoller.java
@@ -1,9 +1,19 @@
 package com.puentes.elibrary.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by Matthew on 3/27/2018.
  *
  * @author Matthew
  */
+
+@Controller
 public class HomeContoller {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 }
